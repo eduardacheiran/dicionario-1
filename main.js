67edc51cc4a298e99dd8a510f5eef1f8b95cc6e7
eduardacheiran <133910877+eduardacheiran@users.noreply.github.com>
2023-12-06 15:29:45 -0300

    Dicionário em Inglês

---
+++ main.js
@@ -0,0 +1,96 @@
+
+const palavra = document.querySelectorAll('.palavra')
+const modal = document.querySelector('.palavras-div')
+const modal0 = document.querySelector('#palavras-div')
+const modal1 = document.querySelector('#palavras-div1')
+const modal2 = document.querySelector('#palavras-div2')
+const modal3 = document.querySelector('#palavras-div3')
+const modal4 = document.querySelector('#palavras-div4')
+const modal5 = document.querySelector('#palavras-div5')
+const modal6 = document.querySelector('#palavras-div6')
+const modal7 = document.querySelector('#palavras-div7')
+const modal8 = document.querySelector('#palavras-div8')
+const modal9 = document.querySelector('#palavras-div9')
+const texto = document.querySelectorAll('#modal')
+const fechar = document.querySelectorAll('.fechar')
+
+
+palavra[0].addEventListener('click',() =>{
+    modal0.style.display = 'block'
+})
+
+palavra[1].addEventListener('click',() =>{
+    modal1.style.display = 'block'
+})
+
+palavra[2].addEventListener('click',() =>{
+    modal2.style.display = 'block'
+})
+
+palavra[3].addEventListener('click',() =>{
+    modal3.style.display = 'block'
+})
+
+palavra[4].addEventListener('click',() =>{
+    modal4.style.display = 'block'
+})
+
+palavra[5].addEventListener('click',() =>{
+    modal5.style.display = 'block'
+})
+
+palavra[6].addEventListener('click',() =>{
+    modal6.style.display = 'block'
+})
+
+palavra[7].addEventListener('click',() =>{
+    modal7.style.display = 'block'
+})
+
+palavra[8].addEventListener('click',() =>{
+    modal8.style.display = 'block' 
+})
+
+palavra[9].addEventListener('click',() =>{
+    modal9.style.display = 'block' 
+})
+
+fechar[0].addEventListener('click', ()=>{
+     modal0.style.display= 'none'
+})
+
+fechar[1].addEventListener('click', ()=>{
+    modal1.style.display= 'none'
+})
+
+fechar[2].addEventListener('click', ()=>{
+    modal2.style.display= 'none'
+})
+
+fechar[3].addEventListener('click', ()=>{
+    modal3.style.display= 'none'
+})
+
+fechar[4].addEventListener('click', ()=>{
+    modal4.style.display= 'none'
+})
+
+fechar[5].addEventListener('click', ()=>{
+    modal5.style.display= 'none'
+})
+
+fechar[6].addEventListener('click', ()=>{
+    modal6.style.display= 'none'
+})
+
+fechar[7].addEventListener('click', ()=>{
+    modal7.style.display= 'none'
+})
+
+fechar[8].addEventListener('click', ()=>{
+    modal8.style.display= 'none'
+})
+
+fechar[9].addEventListener('click', ()=>{
+    modal9.style.display= 'none'
+})
